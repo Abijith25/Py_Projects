bfa0bd780bbe7f3bc1db7e8816d50ca6f63e56fa
Abijith25 <154227751+Abijith25@users.noreply.github.com>
2025-06-11 01:10:41 -0500

    Initialized new branch

--- AREA OF TRIANGLE.py
+++
@@ -1,4 +0,0 @@
-print("FINDING THE AREA OF THE TRIANGLE")
-m,n=map(float,"ENTER THE BASE AND HEIGHT OF THE TRIANGLE".split(","))
-area=(1/2)*(m*n)
-print("AREA OF THE TRIANGLE WITH BASE ",m,"AND HEIGHT",n,"IS ",area)
